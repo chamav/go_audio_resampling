a77c7465d3fcebea1a24e4702e341f5f8fb6c3ac
Vladimir <chamav@gmail.com>
2025-08-12 12:45:02 +0500

    Document resampler and add README

---
+++ cmd/example/main.go
@@ -0,0 +1,38 @@
+// Command example demonstrates resampling a generated sine wave from
+// 44.1 kHz to 48 kHz using the resample package.
+package main
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/example/go_audio_resampling/resample"
+)
+
+func main() {
+	inRate := 44100
+	outRate := 48000
+
+	r, err := resample.New(inRate, outRate)
+	if err != nil {
+		panic(err)
+	}
+	defer r.Close()
+
+	samples := make([]float32, inRate)
+	for i := range samples {
+		samples[i] = float32(math.Sin(2 * math.Pi * 440 * float64(i) / float64(inRate)))
+	}
+
+	out1, err := r.Convert(samples)
+	if err != nil {
+		panic(err)
+	}
+	out2, err := r.Convert(nil)
+	if err != nil {
+		panic(err)
+	}
+	out := append(out1, out2...)
+
+	fmt.Printf("in samples: %d, out samples: %d\n", len(samples), len(out))
+}
